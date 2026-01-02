bf14be5986845d7cb9e7ea3405662ccfafe72eda
Saurabh88sr <saurabh888vns@gmail.com>
2026-01-02 21:59:00 +0530

    change

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Ruzzle Backend is running");
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" },
