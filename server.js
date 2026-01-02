12ba1f3fe30625413eff757575d01c112ce019bc
Saurabh88sr <saurabh888vns@gmail.com>
2026-01-02 23:01:32 +0530

    change port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
 // live and local origins
@@ -16,8 +18,14 @@ app.get("/", (req, res) => {
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: {
+    origin: [
+      "http://localhost:4000",
+      "https://ruzzleboard.vercel.app"
+    ]
+  }
 });
+
 
 const players = {};
 const rooms = {};
@@ -252,6 +260,8 @@ io.on("connection", (socket) => {
   });
 });
 
-// server.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
