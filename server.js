9e1200ee27c03aa64bc6aa66280fc1aea4646daa
Saurabh88sr <saurabh888vns@gmail.com>
2026-01-04 08:32:44 +0530

    PORT env

--- server.js
+++ server.js
@@ -271,8 +271,7 @@ io.on("connection", (socket) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5000; // Use Render's port or default to 5000
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
