f2c9d1d4cb37aca8197919becf372ae42c64e463
Saurabh88sr <saurabh888vns@gmail.com>
2026-01-05 20:47:29 +0530

    url change again

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const io = new Server(server, {
   cors: {
     origin: (origin, callback) => {
       const allowedOrigins = [
-        "http://localhost:5173",
+        "*",
       ];
 
       // polling requests me origin null hota hai
